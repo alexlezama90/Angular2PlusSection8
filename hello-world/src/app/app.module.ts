985577a5516c10c563fe6ec25f55a97d202d7038
Alejandro Lezama <alexlezama90@gmail.com>
2019-06-11 08:52:52 -0500

    initial commit

---
+++ hello-world/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
+import { CourseComponent } from './course/course.component';
+import { CoursesService } from './courses.service';
+import { AutorsComponent } from './autors/autors.component';
+import { SummaryPipe } from './summary.pipe';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { TitleCasePipe } from './title-case.pipe';
+import { PanelComponent } from './panel/panel.component';
+import { LikeComponent } from './like/like.component';
+import { InputFormatDirective } from './input-format.directive';
+import { ZippyComponent } from './zippy/zippy.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CourseComponent,
+    CoursesComponent,
+    AutorsComponent,
+    SummaryPipe,
+    FavoriteComponent,
+    TitleCasePipe,
+    PanelComponent,
+    LikeComponent,
+    InputFormatDirective,
+    ZippyComponent,
+    ContactFormComponent,
+    NewCourseFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [CoursesService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
